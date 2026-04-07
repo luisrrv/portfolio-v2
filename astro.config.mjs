6d62e3a2e3986f3a679089ef39fa3f05eb4e09d1
luisrrv <rodluisr@gmail.com>
2026-04-07 16:55:54 +0900

    basic version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://rodluis.com',
+  build: {
+    inlineStylesheets: 'always', // Inlines all CSS — eliminates render-blocking request
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      cssMinify: true,
+    },
+  },
+});
